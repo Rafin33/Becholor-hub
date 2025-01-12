f0e09fd44f46b9cb819b1b94b465fc0db980bf66
Rafin33 <khanrafin35@gmail.com>
2025-01-13 00:35:50 +0600

    2nd day

---
+++ app/Core/Database.php
@@ -0,0 +1,42 @@
+<?php
+
+class Database
+{
+    private $servername = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $dbname = "bacholor-hub";
+    private $conn;
+
+    // Constructor to initialize the database connection
+    public function __construct()
+    {
+        $this->connect();
+    }
+
+    // Establish a database connection
+    private function connect()
+    {
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+
+        // Check for connection errors
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    // Get the database connection
+    public function getConnection()
+    {
+        return $this->conn;
+    }
+
+    // Close the database connection
+    public function closeConnection()
+    {
+        if ($this->conn) {
+            $this->conn->close();
+        }
+    }
+}
+?>
